6bbb1ccf509529393357dff1009b52467442a1f2
Álvaro <ganzux@users.noreply.github.com>
2017-03-02 16:42:25 +0000

    Create Color.java

---
+++ src/main/java/com/ganzux/paintshop/pojo/Color.java
@@ -0,0 +1,15 @@
+package com.ganzux.paintshop.pojo;
+
+/**
+ * You run a paint shop, and there are a few different colors of paint you can prepare.
+ * Each color can be either "gloss" or "matte".
+ * @throws Exception 
+ */
+public interface Color {
+	
+	public static final String MATTE_LBL = "M";
+	public static final String GLOSS_LBL = "G";
+	
+	public void setName(String text);
+
+}
