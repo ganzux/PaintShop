55fcf04045047e2a2e401daf8e44c5b601c4a9ef
Álvaro <ganzux@users.noreply.github.com>
2017-03-02 16:42:38 +0000

    Create Matte.java

---
+++ src/main/java/com/ganzux/paintshop/pojo/Matte.java
@@ -0,0 +1,15 @@
+package com.ganzux.paintshop.pojo;
+
+public class Matte implements Color{
+
+	public Matte() {
+		super();
+	}
+
+	@Override
+	public void setName(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
