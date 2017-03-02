9db9d7b3ebd715fcc6cd0a6859a1dbd605ea7a85
Álvaro <ganzux@users.noreply.github.com>
2017-03-02 16:42:50 +0000

    Create Gloss.java

---
+++ src/main/java/com/ganzux/paintshop/pojo/Gloss.java
@@ -0,0 +1,15 @@
+package com.ganzux.paintshop.pojo;
+
+public class Gloss implements Color{
+
+	public Gloss() {
+		super();
+	}
+
+	@Override
+	public void setName(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
