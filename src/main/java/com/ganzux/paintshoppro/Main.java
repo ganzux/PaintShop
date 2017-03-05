cd54eeb01ff072c7595827decc08630f3c4596e3
SURFITO\alvaro <itoito@gmail.com>
2017-03-05 19:33:53 +0000

    Final algorithm and some test cases

--- src/main/java/com/ganzux/paintshoppro/Main.java
+++ src/main/java/com/ganzux/paintshoppro/Main.java
@@ -25,15 +25,54 @@ public class Main {
 	 * There is just one batch for each color, and it's either gloss or matte.
 	 * For each customer, there is at least one color they like.
 	 * You make as few mattes as possible (because they are more expensive).
+	 * @throws Exception 
 	 */
-	private static String mix(int numberOfColors, List<Customer> customers){
+	private static String mix(int numberOfColors, List<Customer> customers) throws Exception{
 		String[] colorTypes = new String[numberOfColors];
 		
 		String[][] colorMatrix = generateColorMatrix(numberOfColors, customers);
 		
+		// At this point the matrix is sorted, with those customers with 1 favorite
+		// color at the beginning. Those customers with just 1 color are mandatories
+		for (String[] line : colorMatrix){
+
+			if (isOneValue(line)){
+				for (int idx = 0 ; idx < line.length ; idx++){
+					String value = line[idx];
+					if (null != value){
+						if (colorTypes[idx] == null){
+							colorTypes[idx] = value;
+						} else if (colorTypes[idx] != value){
+							throw new Exception("No solution exists");
+						}
+					}
+				}
+			}
+
+			// There is more than 1 color for the customer
+			else {
+				manageConflicts(line, colorTypes);
+
+				for (int idx = 0 ; idx < line.length ; idx++){
+					String value = line[idx];
+					if (null != value){
+						if (colorTypes[idx] == null){
+							colorTypes[idx] = value;
+						} else if (colorTypes[idx] != value){
+							throw new Exception("It is not possible, sorry!");
+						}
+					}
+				}
+			}
+		}
 		
+		StringBuilder sb = new StringBuilder();
+		for (String s : colorTypes){
+			sb.append(s).append(" ");
+		}
 		
-		return colorTypes.toString();
+		// remove the last white space
+		return sb.substring(0, sb.length() - 1);
 	}
 	
 	private static String[][] generateColorMatrix(int numberOfColors, List<Customer> customers){
@@ -59,16 +98,73 @@ public class Main {
 			// If the Customer is going to mix any color with one that is Gloss,
 			// we can put all of them as gloss; this comes from the first example
 			// provided, 1M 3G 5G - 2G 3M 4G - 5M = G G G G M
-			if (thereIsGloss){
-				for (Color color : customerColors){
-					int colorNumer = Integer.valueOf(color.getName());
-					matrix[customerId][colorNumer - 1] = Color.GLOSS_LBL;
-				}
-			}
+//			if (thereIsGloss){
+//				for (Color color : customerColors){
+//					int colorNumer = Integer.valueOf(color.getName());
+//					matrix[customerId][colorNumer - 1] = Color.GLOSS_LBL;
+//				}
+//			}
 
 			customerId++;
 			}
 		return matrix;
 	}
+	
+	private static boolean isOneValue(String[] line){
+		int values = 0;
+		
+		for (String value : line){
+			if (null != value){
+				values++;
+			}
+			if (values > 1){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	/**
+	 * Method that changes all the Mattes To Gloss if there are not in a certainly position.
+	 * This is because every Customer is allowed to have just 1 favorite Matte color.
+	 * @param line
+	 * @param positionExcluded
+	 * @return true if we changed something
+	 */
+	private static boolean changeMates(String[] line, int positionExcluded){
+		
+		boolean change = false;
+
+		for (int i = 0 ; i < line.length ; i++){
+			if (i != positionExcluded){
+				String value = line[i];
+				if (null != value && Color.MATTE_LBL.equalsIgnoreCase(value)){
+					line[i] = Color.GLOSS_LBL;
+					change = true;
+				}
+			}
+		}
+		
+		return change;
+	}
+
+	private static void manageConflicts(String[] line, String[] solution){
+	
+		for (int i = 0 ; i < line.length ; i++){
+			if (line[i] != null && solution[i] != null && solution[i] != line[i]){
+				if (solution[i] == Color.MATTE_LBL){
+					line[i] = Color.MATTE_LBL;
+					changeMates(line, i);
+				}
+				
+				else {
+					line[i] = Color.GLOSS_LBL;
+				}
+			}
+		}
+
+	}
+
 
 }
--- src/test/java/com/ganzux/paintshoppro/AppTest.java
+++ src/test/java/com/ganzux/paintshoppro/AppTest.java
@@ -18,6 +18,67 @@ public class AppTest extends TestCase {
 		File file = new File(classLoader.getResource(validFolder + "5_colors.3_customers.gilt").toURI());
 		String result = Main.main(file.getAbsolutePath());
 		
-		assertEquals("G G G G M", "G G G G M");
+		assertEquals(result, "G G G G M");
 	}
+	
+	@Test
+	public void testValid5Colors14Customers() throws Exception {
+
+		File file = new File(classLoader.getResource(validFolder + "5_colors.14_customers.gilt").toURI());
+		String result = Main.main(file.getAbsolutePath());
+		
+		assertEquals(result, "G M G M G");
+	}
+	
+	@Test
+	public void testValid2Colors2Customers() throws Exception {
+
+		File file = new File(classLoader.getResource(validFolder + "2_colors.2_customers.gilt").toURI());
+		String result = Main.main(file.getAbsolutePath());
+		
+		assertEquals(result, "M G");
+	}
+	
+	@Test
+	public void testValid9Colors10Customers() throws Exception {
+
+		File file = new File(classLoader.getResource(validFolder + "9_colors.10_customers.gilt").toURI());
+		String result = Main.main(file.getAbsolutePath());
+		
+		assertEquals(result, "G M G M G G M M G");
+	}
+	
+	
+	
+	
+	@Test
+	public void testInValid5Colors3Customers() throws Exception {
+
+		File file = new File(classLoader.getResource(invalidFolder + "1_color.2_customers.gilt").toURI());
+		try{
+			String result = Main.main(file.getAbsolutePath());
+		} catch(Exception e){
+			assertEquals(e.getMessage(), "No solution exists");
+		}
+		
+		
+	}
+	
+	@Test
+	public void testInValid5Colors3CustomersMjultipleMM() throws Exception {
+
+		File file = new File(classLoader.getResource(invalidFolder + "5_colors.3_customers.MM.gilt").toURI());
+		try{
+			String result = Main.main(file.getAbsolutePath());
+		} catch(Exception e){
+			assertEquals(e.getMessage(), "It is not possible, sorry!");
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
 }
