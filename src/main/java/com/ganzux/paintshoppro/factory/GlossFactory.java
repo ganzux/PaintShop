25545eccc5837ec55b2034a791ad065af57bd6aa
Álvaro <ganzux@users.noreply.github.com>
2017-03-06 10:27:25 +0000

    Update GlossFactory.java
    Some comments, refactor of Color interface and new tests and Exceptions

--- src/main/java/com/ganzux/paintshoppro/factory/GlossFactory.java
+++ src/main/java/com/ganzux/paintshoppro/factory/GlossFactory.java
@@ -1,13 +1,22 @@
 package com.ganzux.paintshoppro.factory;
 
-import com.ganzux.paintshoppro.pojo.Color;
+import com.ganzux.paintshoppro.pojo.IColor;
 import com.ganzux.paintshoppro.pojo.Gloss;
 
+/**
+ * Gloss Factory
+ * @author ganzux Alvaro Alcedo Moreno
+ */
 public class GlossFactory implements AbstractColorFactory{
 
+	///////////////////////////////////////////////////////////////
+	//                       Public Methods                      //
+	///////////////////////////////////////////////////////////////
 	@Override
-	public Color createColor() {
+	public IColor createColor() {
 		return new Gloss();
 	}
-
+	///////////////////////////////////////////////////////////////
+	//                      /Public Methods                      //
+	///////////////////////////////////////////////////////////////
 }
