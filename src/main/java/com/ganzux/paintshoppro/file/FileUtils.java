8bef979cfc065f474ae8251653e9cad25d8432ee
Álvaro <ganzux@users.noreply.github.com>
2017-03-06 10:26:10 +0000

    Update FileUtils.java
    Some comments, refactor of Color interface and new tests and Exceptions

--- src/main/java/com/ganzux/paintshoppro/file/FileUtils.java
+++ src/main/java/com/ganzux/paintshoppro/file/FileUtils.java
@@ -2,67 +2,92 @@ package com.ganzux.paintshoppro.file;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.ganzux.paintshoppro.factory.ColorFactory;
-import com.ganzux.paintshoppro.pojo.Color;
 import com.ganzux.paintshoppro.pojo.Customer;
+import com.ganzux.paintshoppro.pojo.IColor;
 
 /**
- * Your program should accept an input file as a command line argument,
- * and print a result to standard out.  An example input file is:
-		5
-		1 M 3 G 5 G
-		2 G 3 M 4 G
-		5 M
+ * Class to manage the Files and bind the data on a Customer Object.
+ * @author ganzux Alvaro Alcedo Moreno
  *
  */
 public abstract class FileUtils {
 
-	public static Object[] readFile(File file) throws Exception{
+	///////////////////////////////////////////////////////////////
+	//                       Public Methods                      //
+	///////////////////////////////////////////////////////////////
+
+	/**
+	 * Method to convert the file into our Data Model Object
+	 * @param file The file to read
+	 * @return An Object with the first position as the number of colors
+	 * and the second one with the Customer Objects
+	 * @throws Exception The Exceptions to be managed
+	 */
+	public static Object[] readFile(File file) throws FileNotFoundException, IOException, Exception {
+
 		int colorNumbers = 0;
 		int numOFCustomers = 0;
-		List<Customer> customers = new ArrayList<Customer>();
 		
-		try {
+		// SecurityCheck with the number of colors
+		Map<String, String> colorsIncluded = new HashMap<String, String>();
 
-			FileReader fileReader = new FileReader(file);
+		List<Customer> customers = new ArrayList<Customer>();
 
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
+		FileReader fileReader = new FileReader(file);
 
-			String line;
-			
-			while ((line = bufferedReader.readLine()) != null) {
-				if (numOFCustomers == 0){
-					try {
-						colorNumbers = Integer.parseInt(line.trim());
-					} catch(NumberFormatException nfe){
-						throw new Exception("First line should be a number!", nfe);
-					}
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+		String line;
+		
+		while ((line = bufferedReader.readLine()) != null) {
+			if (numOFCustomers == 0){
+				try {
+					colorNumbers = Integer.parseInt(line.trim());
+				} catch(NumberFormatException nfe){
+					throw new Exception("First line should be a number!", nfe);
 				}
-				else {
-					customers.add(readPairs(line));
-				}
-				numOFCustomers ++;
 			}
-			fileReader.close();
+			else {
+				customers.add(readPairs(line, colorsIncluded));
+			}
+			numOFCustomers ++;
+		}
 
-		} catch (IOException e) {
-			e.printStackTrace();
+		fileReader.close();
+		
+		if (colorNumbers != colorsIncluded.size()){
+			throw new Exception("The number of colors doesn't match with the specified with the Customers");
 		}
 		
 		return new Object[]{colorNumbers ,customers};
 	}
-	
+
+	/**
+	 * Works like {@link #readFile(File)}
+	 */
 	public static Object[] readFile(String path) throws Exception{
 		File file = new File(path);
 		return readFile(file);
 	}
-	
-	private static Customer readPairs(String str) throws Exception{
+	///////////////////////////////////////////////////////////////
+	//                      /Public Methods                      //
+	///////////////////////////////////////////////////////////////
+
+
+
+	///////////////////////////////////////////////////////////////
+	//                      Private Methods                      //
+	///////////////////////////////////////////////////////////////
+	private static Customer readPairs(String str, Map<String, String> colorsIncluded) throws Exception{
 
 		Customer customer = new Customer();
 
@@ -72,13 +97,17 @@ public abstract class FileUtils {
 		for (int i = 0 ; i < data.length ; i += 2){
 			String colorNumber = data[i];
 			String colorType   = data[i + 1];
+			
+			colorsIncluded.put(colorNumber, colorNumber);
 
-			Color myColor = ColorFactory.createColor(colorType);
+			IColor myColor = ColorFactory.createColor(colorType);
 			myColor.setName(colorNumber);
 			customer.addFavouriteColor(myColor);
 		}
 		
 		return customer;
 	}
-
+	///////////////////////////////////////////////////////////////
+	//                     /Private Methods                      //
+	///////////////////////////////////////////////////////////////
 }
