4a44b560e6420c909c2bb57c9bdf55f5b4d78442
Álvaro <ganzux@users.noreply.github.com>
2017-03-06 10:27:32 +0000

    Update MatteFactory.java
    Some comments, refactor of Color interface and new tests and Exceptions

--- src/main/java/com/ganzux/paintshoppro/factory/MatteFactory.java
+++ src/main/java/com/ganzux/paintshoppro/factory/MatteFactory.java
@@ -1,13 +1,22 @@
 package com.ganzux.paintshoppro.factory;
 
-import com.ganzux.paintshoppro.pojo.Color;
+import com.ganzux.paintshoppro.pojo.IColor;
 import com.ganzux.paintshoppro.pojo.Matte;
 
+/**
+ * Matte Factory
+ * @author ganzux Alvaro Alcedo Moreno
+ */
 public class MatteFactory implements AbstractColorFactory{
 
+	///////////////////////////////////////////////////////////////
+	//                       Public Methods                      //
+	///////////////////////////////////////////////////////////////
 	@Override
-	public Color createColor() {
+	public IColor createColor() {
 		return new Matte();
 	}
-
+	///////////////////////////////////////////////////////////////
+	//                      /Public Methods                      //
+	///////////////////////////////////////////////////////////////
 }
